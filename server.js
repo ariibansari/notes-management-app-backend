b726350a1053ad7ed127de548b5f84b22416f928
ariibansari <89924568+ariibansari@users.noreply.github.com>
2022-10-20 22:26:14 +0530

    added hostname

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+const hostname = '0.0.0.0';
 
 const app = express();
 app.use(cors())
@@ -9,6 +10,6 @@ app.get('/', (req, res) => {
     res.send('<h1>Node Server is Live<h1>');
 })
 
-app.listen(PORT, ()=> {
+app.listen(PORT, hostname, ()=> {
     console.log(`Server started on port ${PORT}`)
 })
